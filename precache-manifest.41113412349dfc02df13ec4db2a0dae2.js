cbd6a9da670bd906c1456f0c7766a113001b5edb
Кочарян Григорий <grigoriy.kocharyan@gazprombank.ru>
2021-04-26 10:49:25 +0300

    update project

---
+++ precache-manifest.41113412349dfc02df13ec4db2a0dae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96bc9ceafeb7d5386428e48d897450eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebbc92db9f441530e294",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "8d32f6cd5f50417ab26d",
+    "url": "/static/css/main.63d1843a.chunk.css"
+  },
+  {
+    "revision": "ebbc92db9f441530e294",
+    "url": "/static/js/2.bf01888d.chunk.js"
+  },
+  {
+    "revision": "76d6d4cba64835378493adbd73366656",
+    "url": "/static/js/2.bf01888d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d32f6cd5f50417ab26d",
+    "url": "/static/js/main.b504f881.chunk.js"
+  },
+  {
+    "revision": "01d52cdac340a573e444",
+    "url": "/static/js/runtime-main.23fe3fbc.js"
+  }
+]);
